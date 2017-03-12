772c3e3a49118b3a141057df245c7aff222c049f
Hexray <zikfrostfire@gmail.com>
2017-03-12 13:22:28 +0200

    Transferred image processing into another thread. Added possibility to change threshold maximal value in runtime

--- SpearCV/Source.cpp
+++ SpearCV/Source.cpp
@@ -1,16 +1,30 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <stdio.h>
+#include <thread>
+using namespace cv;
+using namespace std;
 
 //#include <opencv2/bioinspired.hpp> // Подключаем модуль
 
 //cv::Ptr<cv::bioinspired::Retina> cvRetina; // Модуль сетчатки глаза
 
-using namespace cv;
-using namespace std;
+//Definition of prototypes
+void imgProcessing(double& thresholdMaxValue);
 
 int main()
 {
+	double thresholdMaxValue = 20;
+	thread t = thread(imgProcessing, std::ref(thresholdMaxValue));
+	while (true)
+	{
+		cin >> thresholdMaxValue;
+	}
+
+	return 0;
+}
+
+void imgProcessing(double& thresholdMaxValue) {
 	Mat frame;
 	//--- INITIALIZE VIDEOCAPTURE
 	VideoCapture cap;
@@ -19,10 +33,9 @@ int main()
 	// check if we succeeded
 	if (!cap.isOpened()) {
 		cerr << "ERROR! Unable to open camera\n";
-		return -1;
+		return;
 	}
-
-	while(true)
+	while (true)
 	{
 		// wait for a new frame from camera and store it into 'frame'
 		cap.read(frame);
@@ -31,10 +44,12 @@ int main()
 			cerr << "ERROR! blank frame grabbed\n";
 			break;
 		}
+
+		cvtColor(frame, frame, CV_RGB2GRAY);
+		adaptiveThreshold(frame, frame, thresholdMaxValue, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY, 3, 5);
 		// show live and wait for a key with timeout long enough to show images
 		imshow("Live", frame);
-		waitKey(500);
+		waitKey(50);
 	}
 	// the camera will be deinitialized automatically in VideoCapture destructor
-	return 0;
 }
